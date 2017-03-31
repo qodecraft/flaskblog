04f465f0c6c80cc99f254ee7e51375357a8f77e1
Karan Kyanam <karan.kyanam@gmail.com>
2017-03-31 17:26:54 -0400

    Add feature

--- blog.py
+++ blog.py
@@ -1,5 +1,7 @@
-from flask import Flask, render_template, session, \
+from flask import Flask, render_template, request, session, \
 	flash, redirect, url_for, g
+
+from functools import wraps
 
 import sqlite3
 
@@ -15,21 +17,55 @@ app.config.from_object(__name__)
 def connect_db():
 	return sqlite3.connect(app.config['DATABASE'])
 
-@app.route("/", methods=['GET', 'POST'])
+def login_required(test):
+	@wraps(test)
+	def wrap(*args, **kwargs):
+		if 'logged_in' in session:
+			return test(*args, **kwargs)
+		else:
+			flash('You need to log in first')
+			return redirect(url_for('login'))
+	return wrap
+
+@app.route('/', methods=['GET', 'POST'])
 def login():
 	error = None
 	status_code = 200
 	if request.method == 'POST':
 		if request.form['username'] != app.config['USERNAME'] or request.form['password'] != app.config['PASSWORD']:
 			error = 'Invalid Credentials, Please try agian'
+			status_code = 401
 		else:
 			session['logged_in'] = True
 			return redirect(url_for('main'))
 	return render_template('login.html', error = error), status_code
 
 @app.route('/main')
+@login_required
 def main():
-	return render_template('main.html')
+	g.db = connect_db()
+	cur = g.db.execute('SELECT * from posts')
+	posts = [dict(title=row[0], post=row[1]) for row in cur.fetchall()]
+	g.db.close()
+	return render_template('main.html', posts=posts)
+
+@app.route('/add', methods=['POST'])
+@login_required
+def add():
+	title = request.form['title']
+	post = request.form['post']
+	if not title or not post:
+		flash("All fields are required. Please try agian")
+		return redirect(url_for('main'))
+	else:
+		g.db = connect_db()
+		g.db.execute('insert into posts(title, post) values(?, ?)', 
+			[request.form['title'], request.form['post']])
+		g.db.commit()
+		g.db.close()
+		flash('New entery was succesfully posted')
+		return redirect(url_for('main'))
+
 
 @app.route('/logout')
 def logout():
